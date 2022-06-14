1d93560ee811e41ddbc2971f2575eee4eafef5fc
masato_c0a2115063 <c0a2115063@edu.teu.ac.jp>
2022-06-14 16:08:14 +0900

    まだ制作中

---
+++ ex01/quiz.py
@@ -0,0 +1,20 @@
+import random 
+def main():
+    correct = shutudai()
+    kaito(correct) 
+
+def shutudai():
+    question_ans1 = ["マスオ","ますお"]               # サザエの旦那の名前の回答例
+    question_ans2 = ["ワカメ","わかめ"]               #カツオの妹の名前の回答例
+    question_ans3 = ["甥","おい","甥っ子","おいっこ"] #タラオはカツオから見てどんな関係
+    question_list = ["サザエの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
+    print("問題：")
+    Question = random.randint(question_list)
+    print(Question)
+
+def kaito(correct):
+    answer  = input("答えよ")
+    if answer in correct:
+        print("正解！")
+    else:
+        print("出直してこい")
