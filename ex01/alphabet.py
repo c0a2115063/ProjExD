a124f46caeeb1a28172f9f5fe413923c42da054d
masato_c0a2115063 <c0a2115063@edu.teu.ac.jp>
2022-06-14 18:39:37 +0900

    修正して実行したら理想の形になりました#4

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -3,9 +3,8 @@ TaishouMoji = 10
 KessonMoji  = 2
 #グローバル変数として定義
 Alphabet = [chr(i) for i in range(65,91)]
-random.shuffle(Alphabet)
+Alphabet_random =  random.sample(Alphabet, k = TaishouMoji)
 
 print("対象文字：")
+print(Alphabet_random)
 #アルファベットの配列をランダムで10回出力
-for i in range(10):
-    print(Alphabet)
