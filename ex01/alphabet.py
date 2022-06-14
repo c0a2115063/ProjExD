9c2a7783d1990e299bcb26eb20fafd3ad58e4f45
masato_c0a2115063 <c0a2115063@edu.teu.ac.jp>
2022-06-14 18:47:11 +0900

    時間がなかったため表示文字だけソースコードを追加しました#5

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -7,4 +7,9 @@ Alphabet_random =  random.sample(Alphabet, k = TaishouMoji)
 
 print("対象文字：")
 print(Alphabet_random)
-#アルファベットの配列をランダムで10回出力
+#ランダムされたアルファベットの１０文字をリスト型で出力
+
+hyouji = random.sample(Alphabet_random,8)
+print("表示文字")
+print(hyouji)
+#表示文字だけ書けるように追加した
