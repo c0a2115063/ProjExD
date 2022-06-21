5c4c99937d1a35a4d22e2bd974ff43b2d8e12b20
masato_c0a2115063 <c0a2115063@edu.teu.ac.jp>
2022-06-21 14:40:25 +0900

    問題３まで書きました

---
+++ ex02/calc.py
@@ -0,0 +1,25 @@
+from logging import root
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+r = 0 #行番号
+c = 0 #列番号
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt, f"{txt}のボタンがクリックされました") 
+if __name__ == "__main__":
+    root  = tk.Tk()
+    root.title("電卓")
+    root.geometry("300x500")
+    #list = [9,8,7,6,5,4,3,2,1,0]
+    for i in range(9,-1,-1):
+        button = tk.Button(root, text=i, width=4, height=2, font=("Times New Roman", 30))
+        button.bind("<1>",button_click)
+        button.grid(row=r,column=c)
+        c += 1
+        if (i-1) % 3 == 0:
+            r += 1
+            c =0
+    
+    root.mainloop()
