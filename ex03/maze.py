7ab62e33955989a5596a5d7290d0cbe25b0b30e3
masato_c0a2115063 <c0a2115063@edu.teu.ac.jp>
2022-06-28 14:38:41 +0900

    練習２と練習３

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 
-if __name__ == "__main_":
+if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+
+    Canvas = tk.Canvas(root,width=1500,height=900,bg="black")
+    Canvas.pack()
+
+    bird = tk.PhotoImage(file="fig/6.png")
+    cx,cy = 300,400
+    Canvas.create_image(cx,cy,image=bird,tag="bird")
+    
     root.mainloop()
