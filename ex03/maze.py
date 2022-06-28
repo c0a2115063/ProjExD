304633b1082da19ec6ecb44b6a76959d8448144d
masato_c0a2115063 <c0a2115063@edu.teu.ac.jp>
2022-06-28 18:00:31 +0900

    コメント#13に対する修正

--- ex03/maze.py
+++ ex03/maze.py
@@ -13,23 +13,23 @@ def main_proc():
     global cx,cy,mx,my
     if key == "Up" and maze_bg[my-1][mx] == 0:
         my -= 1
-        Canvas.delete("bird")
-        Canvas.create_image(cx,cy,image=bird_up,tag="bird")
+        Canvas.delete("bird")                               #画像更新で処理が遅くなってしまうため、前の画像を削除する
+        Canvas.create_image(cx,cy,image=bird_up,tag="bird") #上に移動する画像を出力
 
     if key == "Down" and maze_bg[my+1][mx] == 0:
         my += 1
-        Canvas.delete("bird")
-        Canvas.create_image(cx,cy,image=bird_down,tag="bird")
+        Canvas.delete("bird")                               #画像更新で処理が遅くなってしまうため、前の画像を削除する
+        Canvas.create_image(cx,cy,image=bird_down,tag="bird")#下に移動する画像を出力
 
     if key == "Left" and maze_bg[my][mx-1] == 0:
         mx -= 1
-        Canvas.delete("bird")
-        Canvas.create_image(cx,cy,image=bird_left,tag="bird")
+        Canvas.delete("bird")                               #画像更新で処理が遅くなってしまうため、前の画像を削除する
+        Canvas.create_image(cx,cy,image=bird_left,tag="bird")#左に移動する画像を出力
 
     if key == "Right" and maze_bg[my][mx+1] == 0:
         mx += 1
-        Canvas.delete("bird")
-        Canvas.create_image(cx,cy,image=bird_right,tag="bird")
+        Canvas.delete("bird")                               #画像更新で処理が遅くなってしまうため、前の画像を削除する
+        Canvas.create_image(cx,cy,image=bird_right,tag="bird")#右に移動する画像を出力
 
     else:
         pass
@@ -47,10 +47,10 @@ if __name__ == "__main__":
     maze_bg = mm.make_maze(15,9)#壁：1 / 床：2を表す二次元リスト
     mm.show_maze(Canvas,maze_bg)#canvasにmaze_bgを描く
     bird = tk.PhotoImage(file="fig/0.png")
-    bird_up = tk.PhotoImage(file="fig/3.png")
-    bird_down = tk.PhotoImage(file="fig/4.png")
-    bird_left = tk.PhotoImage(file="fig/5.png")
-    bird_right = tk.PhotoImage(file="fig/2.png")
+    bird_up = tk.PhotoImage(file="fig/3.png")  #上に移動するときのこうかとんの画像
+    bird_down = tk.PhotoImage(file="fig/4.png")#下に移動するときのこうかとんの画像
+    bird_left = tk.PhotoImage(file="fig/5.png")#左に移動するときのこうかとんの画像
+    bird_right = tk.PhotoImage(file="fig/2.png")#右に移動するときのこうかとんの画像
 
     Canvas.create_rectangle(100,100,200,200,fill='red')
     Canvas.create_rectangle(1300,700,1400,800,fill='blue')
