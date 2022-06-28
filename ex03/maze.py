ccfae9aafab9834d12802d61bbde806a88d26e94
masato_c0a2115063 <c0a2115063@edu.teu.ac.jp>
2022-06-28 14:49:18 +0900

    練習６

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,10 @@ def key_down(event):
     global key
     key = event.keysym
 
+def key_up(event):
+    global key
+    key = ""
+
 if __name__ == "__main__":
     key = ""
     root = tk.Tk()
@@ -16,4 +20,5 @@ if __name__ == "__main__":
     cx,cy = 300,400
     Canvas.create_image(cx,cy,image=bird,tag="bird")
     root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
     root.mainloop()
