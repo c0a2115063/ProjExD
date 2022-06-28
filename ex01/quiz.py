93160d1ff6a62a96b039dfe52ffbd84ea819d868
masato_c0a2115063 <c0a2115063@edu.teu.ac.jp>
2022-06-28 17:08:41 +0900

    昔

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -4,13 +4,11 @@ def main():
     kaito(correct) 
 
 def shutudai():
-    question_ans1 = ["マスオ","ますお"]               # サザエの旦那の名前の回答例
-    question_ans2 = ["ワカメ","わかめ"]               #カツオの妹の名前の回答例
-    question_ans3 = ["甥","おい","甥っ子","おいっこ"] #タラオはカツオから見てどんな関係
-    question_list = ["サザエの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
+    question_ans = [{"q":"サザエの旦那の名前は？","a": ["マスオ","ますお","鱒男"]},{"q":"カツオの妹の名前は?","a":["ワカメ","わかめ"]},{"q": "タラオはカツオから見てどんな関係?","a":["甥","おい","甥っ子","おいっこ"]}]
     print("問題：")
-    Question = random.randint(question_list)
-    print(Question)
+    r = random.randint(0,2) 
+    print(question_ans[r]["q"])
+    return question_ans[r]["a"]
 
 def kaito(correct):
     answer  = input("答えよ")
@@ -18,3 +16,6 @@ def kaito(correct):
         print("正解！")
     else:
         print("不正解！")
+
+if __name__ == "__main__":
+    main()
