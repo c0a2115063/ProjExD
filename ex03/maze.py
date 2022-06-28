3db6dc40a2a07c6d7fcd8167e96dcb8a619bcdce
masato_c0a2115063 <c0a2115063@edu.teu.ac.jp>
2022-06-28 14:57:43 +0900

    練習７

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,3 +1,4 @@
+from glob import glob
 import tkinter as tk
 
 def key_down(event):
@@ -7,6 +8,22 @@ def key_down(event):
 def key_up(event):
     global key
     key = ""
+
+def main_poc():
+    global cx,cy
+    if key == "Up":
+        cy -= 20
+    if key == "Down":
+        cy += 20
+    if key == "Left":
+        cx -= 20
+    if key == "Right":
+        cx += 20
+    else:
+        pass
+
+    Canvas.coords("bird",cx,cy)
+    root.after(100,main_poc)
 
 if __name__ == "__main__":
     key = ""
@@ -21,4 +38,5 @@ if __name__ == "__main__":
     Canvas.create_image(cx,cy,image=bird,tag="bird")
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
+    main_poc()
     root.mainloop()
